83ba0468b43f41c521d5b05a846892639a7c8d07
root <root@localhost.localdomain>
2021-03-25 13:04:44 -0400

    Press

---
+++ app/src/main/java/com/beteiserichmond/richmond/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,31 @@
+package com.beteiserichmond.richmond.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.beteiserichmond.richmond.R
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val webb2= root.findViewById<WebView>(R.id.Webrich2)
+        webb2.loadUrl("https://www.google.com")
+        return root
+
+    }
+}
---
+++ app/src/main/java/com/beteiserichmond/richmond/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,32 @@
+package com.beteiserichmond.richmond.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.beteiserichmond.richmond.R
+
+class SlideshowFragment : Fragment() {
+
+    private lateinit var slideshowViewModel: SlideshowViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        slideshowViewModel =
+                ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+
+        val webb3= root.findViewById<WebView>(R.id.Webrich3)
+        webb3.loadUrl("https://student.busitema.ac.ug")
+
+        return root
+    }
+}
